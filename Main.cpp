f1b395513cdbc36f28ee6ae17b5b978a489efb97
BetBrest <anich2@mail.ru>
2016-04-22 23:05:26 +0300

    CRC
    Подсчет CRC для бинарных чисел, работа с датой временем

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 //---------------------------------------------------------------------------
 
 #include <vcl.h>
+#include <DateUtils.hpp>
 #pragma hdrstop
 
 #include "Main.h"
@@ -118,40 +119,77 @@ void __fastcall TForm1::Button3Click(TObject *Sender)
 
 
 void __fastcall TForm1::packet_parsing(unsigned char* s, int d)
-{
-TDateTime CounterTime;
+ {
+ TDateTime CounterTime,Time_now ;
+;
 
-//Get Time/Date Counter
-AnsiString sec=IntToHex(s[0],2);
-AnsiString min=IntToHex(s[1],2);
-AnsiString hour=IntToHex(s[2],2);
-AnsiString day=IntToHex(s[4],2);
-AnsiString month=IntToHex(s[5],2);
-AnsiString year=IntToHex(s[6],2);
+ //Get Time/Date Counter
+ AnsiString sec=IntToHex(s[0],2);
+ AnsiString min=IntToHex(s[1],2);
+ AnsiString hour=IntToHex(s[2],2);
+ AnsiString day=IntToHex(s[4],2);
+ AnsiString month=IntToHex(s[5],2);
+ AnsiString year=IntToHex(s[6],2);
 
-//CounterTime.;
-//settings.ShortDateFormat = "mmddyy";
-//AnsiString ss=/*hour + ":" + min + ":" + sec + " " ;//+ */day+ "-"  + month + "-" + year;
-//ShortDateFormat = "dd-mm-yy";
-ShortDateFormat = "dd.mm.yyyy";// - задаешь формат даты
-ShortTimeFormat = "hh:mm:ss";
-AnsiString ss;
-   
- ss = day+ "."  + month + "." + year;
-  // CounterTime = StrToDateTime(ss);
-CounterTime=StrToDate(ss);
+ AnsiString ss;
 
+ ss = day+ "."  + month + "." + year + " " + hour + ":" + min + ":" + sec;
+ CounterTime=StrToDateTime(ss);
 
- ss = hour + ":" + min + ":" + sec;
- CounterTime = StrToTime(ss);
- ShowMessage(CounterTime);
-
- /*ss = day+ "."  + month + "." + year;
-  // CounterTime = StrToDateTime(ss);
-CounterTime=StrToDate(ss);   */
-
-    Edit4->Text=CounterTime.TimeString();
-    Edit5->Text=CounterTime.DateString();
+ Edit4->Text=CounterTime.TimeString();
+ Edit5->Text=CounterTime.DateString();
+ //double difference=Time_now.CurrentDateTime() - CounterTime ;
+  //Edit6->Text=difference;
+Edit7->Text =  FloatToStr(DaysBetween(Time_now.CurrentDateTime(), CounterTime))  ;
+Edit6->Text =  FloatToStr(SecondsBetween(Time_now.CurrentDateTime(), CounterTime))  ;
 //ShowMessage(CounterTime);
+
+}
+void __fastcall TForm1::Button4Click(TObject *Sender)
+{
+unsigned char test[9] = {0x12,0x16,0x22,0x15,0x22,0x02,0x16,0x89,0x23};
+  Form1->packet_parsing(test,9);
+  char *Data, *Result;                                       // Declare two strings
+
+   Data = "123453";
+   Result = MakeCRC(Data);                                    // Calculate CRC
+   
+   ShowMessage((BinToHex(StrToInt(*Result),2)));
+
+  // return(0);
+
+
+}
+//---------------------------------------------------------------------------
+
+
+char* TForm1::MakeCRC(char *BitString)
+{
+
+   static char Res[9];                                 // CRC Result
+   char CRC[8];
+   int  i;
+   char DoInvert;
+   
+   for (i=0; i<8; ++i)  CRC[i] = 0;                    // Init before calculation
+   
+   for (i=0; i<strlen(BitString); ++i)
+      {
+      DoInvert = ('1'==BitString[i]) ^ CRC[7];         // XOR required?
+
+      CRC[7] = CRC[6];
+      CRC[6] = CRC[5];
+      CRC[5] = CRC[4] ^ DoInvert;
+      CRC[4] = CRC[3] ^ DoInvert;
+      CRC[3] = CRC[2];
+      CRC[2] = CRC[1];
+      CRC[1] = CRC[0];
+      CRC[0] = DoInvert;
+      }
+      
+   for (i=0; i<8; ++i)  Res[7-i] = CRC[i] ? '1' : '0'; // Convert binary to ASCII
+   Res[8] = 0;                                         // Set string terminator
+
+   return(Res);
 
 }
--- Main.h
+++ Main.h
@@ -30,17 +30,20 @@ __published:	// IDE-managed Components
         TEdit *Edit6;
         TEdit *Edit7;
         TLabel *Label5;
+        TButton *Button4;
         void __fastcall Button1Click(TObject *Sender);
         void __fastcall Timer1Timer(TObject *Sender);
         void __fastcall Button2Click(TObject *Sender);
         void __fastcall ComPort1RxChar(TObject *Sender, int Count);
         void __fastcall Button3Click(TObject *Sender);
+        void __fastcall Button4Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
          //void packet_parsing(unsigned char*, int );
         __fastcall TForm1(TComponent* Owner);
 
         void __fastcall packet_parsing(unsigned char*, int);
+        char* MakeCRC(char*);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TForm1 *Form1;
